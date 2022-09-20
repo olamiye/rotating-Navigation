75df88568a74d15a92ee18adc3e31bf091406b40
olamiye <captainjoe550@gmail.com>
2022-09-20 13:20:34 +0100

    finished up transition

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const open = document.getElementById('.open');
-const close = document.getElementById('.close');
+let openBtn = document.getElementById('open');
+let close = document.getElementById('close');
 const container = document.querySelector('.container');
 
-open.addEventListener('click', () => container.classList.add('show-nav'));
-close.addEventListener('click', () => container.classList.remove('show-nav'));
+// // open.addEventListener('click', () => container.classList.add('show-nav'));
+// // close.addEventListener('click', () => container.classList.remove('show-nav'));
+openBtn.addEventListener("click", function(){
+ container.classList.add("show-nav")
+})
+
+close.addEventListener("click", function(){
+    container.classList.remove("show-nav")
+   })
